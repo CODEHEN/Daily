f7095c30190289745db78c28ee189e08bd5d40f3
CODE <420211582@qq.com>
2020-03-22 22:30:04 +0800

    0322 稍微改动

--- src/main/java/com/day/Controller/publicController.java
+++ src/main/java/com/day/Controller/publicController.java
@@ -4,11 +4,9 @@ import com.day.pojo.User;
 import com.day.repositories.UserRepository;
 import net.minidev.json.JSONArray;
 import org.springframework.transaction.annotation.Transactional;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -22,13 +20,12 @@ public class publicController {
     @ResponseBody
     @Transactional
     public JSONArray allStudent(String time){
-        System.out.println(time);
-if (time==""){
+    if (time==""){
     Date date=new Date();
     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
     time=formatter.format(date);
 
-}
+    }
         List<User> users=userRepository.nameStatu();
         List<User> nameStatus=userRepository.ReportStatu(time);
         JSONArray jsonArray = new JSONArray();
---
+++ src/main/java/com/day/pojo/test.java
@@ -0,0 +1,11 @@
+package com.day.pojo;
+
+/**
+ * @author: chenii
+ * @date: 2020/3/22 22:23
+ * @version: 1.0
+ * @desc:
+ */
+public class test {
+    private String id;
+}
